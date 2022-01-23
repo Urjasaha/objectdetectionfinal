c1b67dab139b08879bbf046dc511daacc1a25fe8
Urjasaha <77976149+Urjasaha@users.noreply.github.com>
2022-01-23 18:24:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function A2() {
+
+   window.location = "hourglass.html";
+}
+
+function A1() {
+
+   window.location = "laptop.html";
+}
+
+function A3() {
+
+   window.location = "bottle.html";
+}
